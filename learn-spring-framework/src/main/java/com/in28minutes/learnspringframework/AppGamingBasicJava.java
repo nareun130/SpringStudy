c296aa90897140922c36c245fceb8f3fc42f84ea
김성호 <naruen130@gmail.com>
2023-09-04 17:07:09 +0900

    [FEAT] : game 추가

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -6,8 +6,9 @@ import com.in28minutes.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
     public static void main(String[] args) { 
-        var game = new MarioGame();
+        // var game = new MarioGame();
         // var game = new SuperContraGame();
+        var game = new PackManGame();
         var gameRunnder = new GameRunnder(game);
         gameRunnder.run();
     }
