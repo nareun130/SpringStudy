c4636a632a820a363267aa22825cf0ab131939da
김성호 <naruen130@gmail.com>
2023-09-04 16:51:44 +0900

    [FEAT] : 패키지 변경 확인

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -1,5 +1,8 @@
 package com.in28minutes.learnspringframework;
 
+import com.in28minutes.learnspringframework.game.GameRunnder;
+import com.in28minutes.learnspringframework.game.SuperContraGame;
+
 public class AppGamingBasicJava {
     public static void main(String[] args) {
 
