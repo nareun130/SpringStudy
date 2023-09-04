d7164dc861e4477ad825660840252513094398a1
김성호 <naruen130@gmail.com>
2023-09-04 17:44:26 +0900

    [FEAT] : bean 추가

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -7,11 +7,14 @@ public class App02HelloWorldSpring {
         // * 1. 스프링 컨텍스트 실행 단게 - 설정 클래스를 사용하여 Spring 컨텍스트 시작 가능*/
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
         // * 2. 스프링이 관리해야하는 것을 설정 - @Configuration */
-        //HelloWorldConfiguration - @Configuration
-        //name - @Bean
-        
+        // HelloWorldConfiguration - @Configuration
+        // name - @Bean
+
         // 여러 방법이 있지만 메서드 이름으로 bean을 가져옴.);
         System.out.println(context.getBean("name"));
-        
+        System.out.println(context.getBean("age")); 
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("address"));
     }
 }
+ 
--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,6 +3,9 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//? record :java 클래스를 만들 때 lombok의 @Data 같은  기능 - jdk 16부터 사용가능
+record Person(String name, int age){};
+record Address(String nation, String city){};
 @Configuration
 public class HelloWorldConfiguration {
 
@@ -10,4 +13,20 @@ public class HelloWorldConfiguration {
     public String name(){
         return "nareun";
     }
+
+    @Bean
+    public int age(){
+        return 25;
+    }
+
+    @Bean
+    public Person person(){
+        return new Person("seongho", 25);
+    }
+    @Bean
+    public Address address(){
+        return new Address("Korea", "seoul");
+    }
+
+
 } 
