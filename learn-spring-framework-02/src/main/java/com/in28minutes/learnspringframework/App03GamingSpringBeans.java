ba2eee39078c0de65040db3b25b0081505667e12
김성호 <naruen130@gmail.com>
2023-09-06 02:35:45 +0900

    [FEAT] : GamingConfiguration class를 App으로 넣음.

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/App03GamingSpringBeans.java
@@ -1,9 +1,29 @@
 package com.in28minutes.learnspringframework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.learnspringframework.game.GameRunnder;
 import com.in28minutes.learnspringframework.game.GamingConsole;
+import com.in28minutes.learnspringframework.game.PackManGame;
+
+@Configuration
+class GamingConfiguration {
+    //!수동으로 Bean 생성 -> Spring이 자동으로 Bean 생성하도록 변환!
+    @Bean
+    public GamingConsole game(){
+        var game = new PackManGame(); //* packman뿐만이 아닌 mario나 다른 게임도 받아 주려면 어떻게 해야할까? */
+        return game;
+    }
+
+    @Bean
+    public GameRunnder gameRunnder(GamingConsole game){
+        var gameRunnder = new GameRunnder(game);
+        return gameRunnder;
+    
+    }
+}
 
 public class App03GamingSpringBeans {
     public static void main(String[] args) {
