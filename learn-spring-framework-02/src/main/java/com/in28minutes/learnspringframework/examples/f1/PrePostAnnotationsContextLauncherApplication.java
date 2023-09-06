d5cc78336e3ed806d327a99ca37a4b7f1358e943
김성호 <naruen130@gmail.com>
2023-09-06 20:49:58 +0900

    [FEAT] : 생성자를 통한 의존성 주입 순서 파악

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/f1/PrePostAnnotationsContextLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/f1/PrePostAnnotationsContextLauncherApplication.java
@@ -11,33 +11,36 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 
 @Component
-class SomeClass{
+class SomeClass {
     private SomeDependency someDependency;
 
-    public SomeClass(SomeDependency someDependency){
-        super(); 
+    public SomeClass(SomeDependency someDependency) {
+        super();
         this.someDependency = someDependency;
         System.out.println("All dependencies are ready!");
     }
 
-    @PostConstruct //* 의존성을 연결하는 대로 초기화를 실행 */
-    //~> 다른 Bean이 이 Bean을 사용할 수 있게 되기 전에 이 메서드가 호출됨.
-    //? => DB등에서 데이터를 가져오려는 경우에 사용 가능
-    public void initalize(){
+    @PostConstruct // * 의존성을 연결하는 대로 초기화를 실행 */
+    // ~> 다른 Bean이 이 Bean을 사용할 수 있게 되기 전에 이 메서드가 호출됨.
+    // ? => DB등에서 데이터를 가져오려는 경우에 사용 가능
+    public void initalize() {
         someDependency.getReady();
     }
 
-    @PreDestroy//* 컨텍스트에서 Bean이 삭제되기 전에 실행  */
-    //~> DB등에 연결되어 있다면 cleanUp()으로 종료
-    //?  => 보유하고 있던 리소스를 해제하는데 일반적으로 사용
-    public void cleanUp(){
+    @PreDestroy // * 컨텍스트에서 Bean이 삭제되기 전에 실행 */
+    // ~> DB등에 연결되어 있다면 cleanUp()으로 종료
+    // ? => 보유하고 있던 리소스를 해제하는데 일반적으로 사용
+    public void cleanUp() {
         System.out.println("CleanUp");
     }
 
 }
 
 @Component
-class SomeDependency{
+class SomeDependency {
+    public SomeDependency() {
+        System.out.println("Some logic creating SomeDependency");
+    }
 
     public void getReady() {
         System.out.println("Some logic using SomeDepedency ");
