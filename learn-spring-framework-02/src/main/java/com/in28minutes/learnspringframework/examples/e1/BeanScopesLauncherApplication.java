2839ecfc0b8f238c9c16b0f8cf77a28e70975065
김성호 <naruen130@gmail.com>
2023-09-06 20:37:01 +0900

    [FEAT] : prototype빈과 싱글톤 빈의 비교

--- learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/e1/BeanScopesLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/e1/BeanScopesLauncherApplication.java
@@ -10,12 +10,14 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component//! 기본적으로 Spring에서 생성되는 모든 bean은 싱글톤!!
+//? 사용자 정보가 없거나 일반적이어서 앱 전체에서 사용할 수 있는 경우 -> Stateless Bean 
 class NormalClass {
 
 }
 
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 // ~> 프로토 타입 클래스가 됨. -> Bean 참조 때마다 다른 객체 생성
+//? 사용자 정보가 유지되는 Bean을 만드려고 할 때 사용 -> Stateful Bean
 @Component
 class PrototypeClass {
 
