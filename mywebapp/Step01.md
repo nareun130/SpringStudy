6004c6d24528694d5586b9a050965a87c272e87d
김성호 <naruen130@gmail.com>
2023-09-11 18:23:53 +0900

    [FEAT] : slf4j 로그 찍기 + 모델2 의 이해

--- mywebapp/Step01.md
+++ mywebapp/Step01.md
@@ -18,3 +18,14 @@
 ## Login JSP
 
 / login => com.nareun.springboot.mywebapp.login.LoginController =>login.jsp
+
+##
+localhost:8080/login
+
+1 : DispatcherServlet이 옳바른 컨트롤러 메서드를 식별 
+    /login => LoginController.gotoLoginPage
+2 : 이 컨트롤러 메서드를 실행 ~> 컨트롤러가 필요한 데이터를 모델에 넣고 뷰 이름을 리턴 
+    => login
+3 : View Resolver가 prefix와 suffix설정을 조합하여 적절한 뷰를 찾아줌.
+    => /WEB-INF/jsp/login.jsp
+4 : 뷰 실행
--- mywebapp/src/main/java/com/nareun/springboot/mywebapp/LoginController.java
+++ mywebapp/src/main/java/com/nareun/springboot/mywebapp/LoginController.java
@@ -1,5 +1,7 @@
 package com.nareun.springboot.mywebapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +10,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    // ~> 가장 좋은 방식은 Logger를 이용 -> 로그 수준 설정 가능
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     // / login => com.nareun.springboot.mywebapp.login.LoginController =>login.jsp
 
     // http:localhost:8080/login?name=nareun
-    //* */ Model에다 param값을 넣어서 뷰에서 다룰 것이다.
+    // * */ Model에다 param값을 넣어서 뷰에서 다룰 것이다.
     @RequestMapping("login")
     public String gotoLoginPage(@RequestParam String name, ModelMap model) {
         model.put("name", name);
-        System.out.println("Request param is " + name);//! 프로덕션 어플리케이션에서는 System.out을 쓰지 말자!! ~> 로깅으로 대체
+        logger.debug("Request param is {}", name); //level 1
+        logger.info("I want this printed at info level", name); //level2
+        logger.warn("I want this printed at warn level", name);  //level3
+        System.out.println("Request param is " + name);// !권장 X : 프로덕션 어플리케이션에서는 System.out을 쓰지 말자!! -> 로그 수준이 제어 x
+
         return "login";
 
     }
