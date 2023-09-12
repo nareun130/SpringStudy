79d76fb39c74af5a66afd36190746e83b2800773
김성호 <naruen130@gmail.com>
2023-09-13 04:39:37 +0900

    [FEAT] : todo 기능 구현

--- mywebapp/src/main/java/com/nareun/springboot/mywebapp/todo/TodoController.java
+++ mywebapp/src/main/java/com/nareun/springboot/mywebapp/todo/TodoController.java
@@ -1,11 +1,13 @@
 package com.nareun.springboot.mywebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -34,9 +36,13 @@ public class TodoController {
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.POST)
-    public String addNewTodoPage(){
-        //? 여기서 Model을 받아서 todo에 추가하고 listTodos를 호출하면 작업이 중복되니 리디렉션을 사용
-        //* */ 리디렉션
+    public String addNewTodoPage(@RequestParam String description, ModelMap model) {
+        String username = (String) model.get("name");
+        todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
+
+        // ? 여기서 Model을 받아서 todo에 추가하고 listTodos를 호출하면 아무런 model이 없는 채로 view만 호출
+        // * */-> 리디렉션 사용
         return "redirect:list-todos";
+        // return "listTodos";
     }
 }
--- mywebapp/src/main/java/com/nareun/springboot/mywebapp/todo/TodoService.java
+++ mywebapp/src/main/java/com/nareun/springboot/mywebapp/todo/TodoService.java
@@ -10,11 +10,13 @@ import org.springframework.stereotype.Service;
 public class TodoService {
     private static List<Todo> todos = new ArrayList<>();
 
+    private static int todosCount = 0;
+
     // ! 정적 변수를 초기화 하기 위해 static 블럭이 필요 -> 딱 한 번 수행
     static {
-        todos.add(new Todo(1, "nareun", "Learn AWS", LocalDate.now().plusYears(1), false));
-        todos.add(new Todo(1, "nareun", "Learn DevOps", LocalDate.now().plusYears(2), false));
-        todos.add(new Todo(1, "nareun", "Learn Fullstack DEvelopment", LocalDate.now().plusYears(3), false));
+        todos.add(new Todo(++todosCount, "nareun", "Learn AWS", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(++todosCount, "nareun", "Learn DevOps", LocalDate.now().plusYears(2), false));
+        todos.add(new Todo(++todosCount, "nareun", "Learn Fullstack DEvelopment", LocalDate.now().plusYears(3), false));
     }
 
     public List<Todo> findByUserName(String username) {
@@ -22,4 +24,8 @@ public class TodoService {
         return todos;
     }
 
+    public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+        Todo todo = new Todo(++todosCount, username, description, targetDate, done); 
+        todos.add(todo);
+    }
 }
