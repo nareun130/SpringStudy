cec87036821255f558fd8d041867a69700c0e67c
김성호 <naruen130@gmail.com>
2023-09-13 01:57:13 +0900

    [FEAT] : Todo리스트를 위한 클래스 생성

---
+++ mywebapp/src/main/java/com/nareun/springboot/mywebapp/todo/TodoService.java
@@ -0,0 +1,21 @@
+package com.nareun.springboot.mywebapp.todo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class TodoService {
+    private static List<Todo> todos;
+
+    // ! 정적 변수를 초기화 하기 위해 static 블럭이 필요 -> 딱 한 번 수행
+    static {
+        todos.add(new Todo(1, "nareun", "Learn AWS", LocalDate.now().plusYears(1), false));
+        todos.add(new Todo(1, "nareun", "Learn DevOps", LocalDate.now().plusYears(2), false));
+        todos.add(new Todo(1, "nareun", "Learn Fullstack DEvelopment", LocalDate.now().plusYears(3), false));
+    }
+
+    public List<Todo> findByUserName(String username) {
+
+        return todos;
+    }
+
+}
