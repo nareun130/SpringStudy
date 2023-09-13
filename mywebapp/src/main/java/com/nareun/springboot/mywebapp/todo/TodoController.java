58378d5e5015308f1c64c363bdaa5175579dabc1
김성호 <naruen130@gmail.com>
2023-09-13 16:37:31 +0900

    [FEAT] : Spring Boot Starter Validation 의존성을 추가하여 커맨드 빈을 만듦.

--- mywebapp/src/main/java/com/nareun/springboot/mywebapp/todo/TodoController.java
+++ mywebapp/src/main/java/com/nareun/springboot/mywebapp/todo/TodoController.java
@@ -29,16 +29,25 @@ public class TodoController {
         return "listTodos";
     }
 
-    // GET,POST ~> POST
-    @RequestMapping(value = "add-todo", method = RequestMethod.GET)
-    public String showNewTodoPage() {
+    // GET,POST ~> GET
+    @RequestMapping(value = "add-todo", method = RequestMethod.GET) 
+    public String showNewTodoPage(ModelMap model) {
+        //* */todo등록 페이지에서 todo객체가 있어야 등록이 가능하므로 임시객체를 만들어서 Model에 넣어준다.
+        String username = (String) model.get("name");
+        Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+        model.put("todo", todo);
         return "todo";
     }
 
     @RequestMapping(value = "add-todo", method = RequestMethod.POST)
-    public String addNewTodoPage(@RequestParam String description, ModelMap model) {
+    public String addNewTodo(ModelMap model, Todo todo) {// ! jsp에서도 양식 보조객체를 설정해 줘야 한다.
+        // * */ todo 등록 페이지에 필드가 많으면 계속 RequestParam으로 가져와서 필드를 검사해야한다.
+        // ~> Todo에 직접 바인딩? description값을 직접 Todo.description에 바인딩,
+        // ~> localdate나 done같은 필드를 추가하게 되어도 Bean에 바운딩 될 것이다.
+        // ~> 커맨드 빈 (Todo또는 Todo Bean을 보조객체 or 커맨드 빈으로 사용)
+
         String username = (String) model.get("name");
-        todoService.addTodo(username, description, LocalDate.now().plusYears(1), false);
+        todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
 
         // ? 여기서 Model을 받아서 todo에 추가하고 listTodos를 호출하면 아무런 model이 없는 채로 view만 호출
         // * */-> 리디렉션 사용
--- mywebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
+++ mywebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/todo.jsp
@@ -1,18 +1,21 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %> <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <html>
   <head>
     <link rel="stylesheet" href="webjars/bootstrap/5.1.3/css/bootstrap.min.css" />
     <title>List Todos Page</title>
   </head>
   <body>
-    <div class="container">
+    <div class="container"> 
       <div>Welcome ${name}</div>
       <hr />
       <h1>Enter Todo Details</h1>
-      <form method="post">
-        Description: <input type="text" name="description" />
+      <form:form method="post" modelAttribute="todo">
+        <!-- ! javascript나 html에서의 검증은 보안이 약함. -> 서버 측 검사가 최선! -->
+        Description: <form:input type="text" path="description" required="required" />
+        <form:input type="hidden" path="id" />
+        <form:input type="hidden" path="done" />
         <input type="submit" class="btn btn-success" />
-      </form>
+      </form:form>
       <script src="webjars/bootstrap/5.1.3/js/bootstrap.min.js"></script>
       <script src="webjars/jquery/3.6.0/jquery.min.js "></script>
     </div>
