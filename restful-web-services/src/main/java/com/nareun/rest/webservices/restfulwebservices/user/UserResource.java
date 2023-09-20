c547238eb777ba0bba7edc5bd65a1796bf8da9dd
김성호 <naruen130@gmail.com>
2023-09-20 17:04:08 +0900

    [FEAT] : 사용자 등록 api 추가

--- restful-web-services/src/main/java/com/nareun/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/nareun/rest/webservices/restfulwebservices/user/UserResource.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,16 +16,25 @@ public class UserResource {
         this.service = service;
     }
 
-    // Get /users
+    // GET /users
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
+    // GET /users/{id}
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
-        
+
         return service.findOne(id);
- 
+
+    }
+
+    // POST /users 
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {// * */ 요청 본문은 ReqeustBody에
+        service.save(user);
+
+        //! 리소스 생성할 때는 생성됐다는 상태를 응답으로 보내주는게 좋다.
     }
 }
