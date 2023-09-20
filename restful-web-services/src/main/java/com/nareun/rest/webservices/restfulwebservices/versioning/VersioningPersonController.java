08075a70eaa96199e49f8c7a9315a58cd974d632
김성호 <naruen130@gmail.com>
2023-09-21 03:30:34 +0900

    [FEAT] : REST API 버전관리

---
+++ restful-web-services/src/main/java/com/nareun/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -0,0 +1,57 @@
+package com.nareun.rest.webservices.restfulwebservices.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersioningPersonController {
+    //! 버전관리가 필요하기 전에도 버전 관리에 대해 고려하는 것이 좋다.
+    //! REST API 구축 전에 버전 관리에 대해 고민해야 한다.
+
+    //* REST API 버전관리 -> 어떤 방식을 선택하든지 일관된 방식으로!!!
+    // * 1. URI 버전관리 - Twitter
+    @GetMapping("/v1/person")
+    public PersonV1 getFirstVersionOfPerson() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping("/v2/person")
+    public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    // * 2. 파라미터 버전 관리 - Amazon
+    // /person?version=1
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    // /person?version=2
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondtVersionOfPersonRequestParameter() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    // * 3. 헤더 버전관리 - Microsoft
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+
+    // * 4. 미디어 타입 버전관리 ~> Accept헤더로 받는 경우 - Github
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+}
