cdd4848688dc830ecbbd149869cda9c29086edbf
김성호 <naruen130@gmail.com>
2023-09-20 16:53:14 +0900

    [FEAT] : REST API 이용 사용자 목록, 사용자 조회

--- restful-web-services/src/main/java/com/nareun/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/nareun/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,7 +1,9 @@
 package com.nareun.rest.webservices.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 
 //REST API
 @RestController
@@ -12,5 +14,20 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world") //~> RequestMapping보다 이걸 추천!
     public String helloworld() {
         return "Hello World";
+    }
+    
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+    
+    //* Path Parameters
+    // /users/{id}/todos/{id} => /users/2/todos/200
+    // /hello-world/path-variable/{name}
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
