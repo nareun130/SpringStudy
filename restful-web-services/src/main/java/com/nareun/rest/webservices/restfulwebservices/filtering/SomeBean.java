aca565b261a10b815823b2c03b40259bb8cc2300
김성호 <naruen130@gmail.com>
2023-09-21 20:13:13 +0900

    [FEAT] : 동적 필터링

--- restful-web-services/src/main/java/com/nareun/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ restful-web-services/src/main/java/com/nareun/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,52 +1,54 @@
 package com.nareun.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-// @JsonIgnoreProperties({"filed1","filed2"})//~> 필드 이름 변경되면 수정해줘야 함.
+// @JsonIgnoreProperties({"field1","field2"})//~> 필드 이름 변경되면 수정해줘야 함.
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 
-    private String filed1;
+    private String field1;
 
     // @JsonIgnore
-    private String filed2;
-    @JsonIgnore
-    private String filed3;
+    private String field2;
+    // @JsonIgnore
+    private String field3;
 
-    public SomeBean(String filed1, String filed2, String filed3) {
+    public SomeBean(String field1, String field2, String field3) {
         super();
-        this.filed1 = filed1;
-        this.filed2 = filed2;
-        this.filed3 = filed3;
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
     }
 
-    public String getFiled1() {
-        return filed1;
+    public String getfield1() {
+        return field1;
     }
 
-    public void setFiled1(String filed1) {
-        this.filed1 = filed1;
+    public void setfield1(String field1) {
+        this.field1 = field1;
     }
 
-    public String getFiled2() {
-        return filed2;
+    public String getfield2() {
+        return field2;
     }
 
-    public void setFiled2(String filed2) {
-        this.filed2 = filed2;
+    public void setfield2(String field2) {
+        this.field2 = field2;
     }
 
-    public String getFiled3() {
-        return filed3;
+    public String getfield3() {
+        return field3;
     }
 
-    public void setFiled3(String filed3) {
-        this.filed3 = filed3;
+    public void setfield3(String field3) {
+        this.field3 = field3;
     }
 
     @Override
     public String toString() {
-        return "SomeBean [filed1=" + filed1 + ", filed2=" + filed2 + ", filed3=" + filed3 + "]";
+        return "SomeBean [field1=" + field1 + ", field2=" + field2 + ", field3=" + field3 + "]";
     }
 
 }
