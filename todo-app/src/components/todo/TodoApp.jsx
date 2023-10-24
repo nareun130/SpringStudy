37d7e81e1a2e410e7f3896463725c4ca556ad15a
김성호 <nareun130@gmail.com>
2023-10-24 12:51:07 +0900

    [FEAT] : Todo-App 리액트 라우터 적용. 존재하지 않는 url에 에러페이지 설정

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 import "./TodoApp.css";
 export default function TodoApp() {
   return (
     <div className="TodoApp">
-      <LoginComponent />
-      {/* <WelcomeComponent /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginComponent />}></Route>
+          <Route path="/login" element={<LoginComponent />}></Route>
+          <Route path="/welcome" element={<WelcomeComponent />}></Route>
+          <Route path="*" element={<ErrorComponent />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
@@ -17,6 +24,9 @@ function LoginComponent() {
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
   const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+  const navigate = useNavigate();
+
   function handleUsernameChange(event) {
     setUsername((username) => event.target.value);
   }
@@ -30,28 +40,19 @@ function LoginComponent() {
       console.log("successs");
       setShowSuccessMessage(true);
       setShowErrorMessage(false);
+      navigate("/welcome");
     } else {
       console.log("failed");
       setShowSuccessMessage(false);
       setShowErrorMessage(true);
     }
   }
-  function SuccessMessageComponent() {
-    if (showSuccessMessage) {
-      return <div className="successMessage">Authenticated Successfully</div>;
-    }
-    return null;
-  }
-  function ErrorMessageComponent() {
-    if (showErrorMessage) {
-      return <div className="errorMessage">Authenticated Failed. Please check your credentials.</div>;
-    }
-    return null;
-  }
+
   return (
     <div className="Login">
-      <SuccessMessageComponent />
-      <ErrorMessageComponent />
+      <h1>Time to Login</h1>
+      {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
+      {showErrorMessage && <div className="errorMessage">Authenticated Failed. Please check your credentials.</div>}
       <div className="LoginForm">
         <div>
           <label>UserName:</label>
@@ -72,5 +73,19 @@ function LoginComponent() {
 }
 
 function WelcomeComponent() {
-  return <div className="Welcome">Welcome Component</div>;
+  return (
+    <div className="Welcome">
+      <h1>Welcome nareun</h1>
+      <div>Welcome Component</div>
+    </div>
+  );
+}
+
+function ErrorComponent() {
+  return (
+    <div className="ErrorComponent">
+      <h1>We are working really hard!</h1>
+      <div>Apologies for the 404. Reach out to our team at ABC-DEF-GHIJ.</div>
+    </div>
+  );
 }
