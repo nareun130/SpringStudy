306abbbfe6ff69e1b6f3dd6f6334c25491e22275
김성호 <nareun130@gmail.com>
2023-10-25 19:45:36 +0900

    no message

--- rest-web-services/src/main/java/com/nareun/rest/webservices/RestWebServicesApplication.java
+++ rest-web-services/src/main/java/com/nareun/rest/webservices/RestWebServicesApplication.java
@@ -1,4 +1,4 @@
-package com.nareun.rest.webservices.restwebservices;
+package com.nareun.rest.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
