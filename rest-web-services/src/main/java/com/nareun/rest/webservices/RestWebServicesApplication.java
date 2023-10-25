fdb5740d08b4ecf7a5bc0394d4435a34df473488
김성호 <nareun130@gmail.com>
2023-10-25 21:17:34 +0900

    [FEAT] : 서버 - CORS설정

--- rest-web-services/src/main/java/com/nareun/rest/webservices/RestWebServicesApplication.java
+++ rest-web-services/src/main/java/com/nareun/rest/webservices/RestWebServicesApplication.java
@@ -2,6 +2,7 @@ package com.nareun.rest.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestWebServicesApplication {
@@ -10,4 +11,12 @@ public class RestWebServicesApplication {
 		SpringApplication.run(RestWebServicesApplication.class, args);
 	}
 
+	/*
+	 * 현재 todo-app(http://localhost:3000)에서 서버 포트로 요청을하면
+	 * Cross Orgigin Request가 걸린다.
+	 * http://localhost:3000에서 오는 모든 요청을 허용해주도록 설정해야 한다.
+	 */
+	//~> WebMvcConfigurer를 구현해주어야 한다.
+
 }
+ 
