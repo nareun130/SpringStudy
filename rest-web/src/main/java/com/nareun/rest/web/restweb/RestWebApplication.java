c83883486924248bf883a6ef348e556f36ac2907
김성호 <nareun130@gmail.com>
2023-10-25 19:34:14 +0900

    [FEAT] : Todo-App과 통신할 서버 프로젝트 생성

---
+++ rest-web/src/main/java/com/nareun/rest/web/restweb/RestWebApplication.java
@@ -0,0 +1,13 @@
+package com.nareun.rest.web.restweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWebApplication.class, args);
+	}
+
+}
