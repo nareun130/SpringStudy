151e831587331df04eaf2ee052b4e23cbed5cf1a
김성호 <nareun130@gmail.com>
2023-10-26 22:04:49 +0900

    [FEAT] : todo-app 유효성 검증 추가

--- todo-app/src/components/todo/TodoComponent.jsx
+++ todo-app/src/components/todo/TodoComponent.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { retrieveTodoApi } from "./api/TodoApiService";
 import { useAuth } from "./security/AuthContext";
-import { Field, Formik, Form } from "formik";
+import { Field, Formik, Form, ErrorMessage } from "formik";
+import { validate } from "json-schema";
 
 export default function TodoComponent() {
   const { id } = useParams();
@@ -23,16 +24,38 @@ export default function TodoComponent() {
       .catch((error) => console.log(error));
   }
   //* Form으 value들을 가져온다.
-  function onSubmit(values){
+  function onSubmit(values) {
     console.log(values);
+  }
+
+  function validate(values) {
+    const targetDate = new Date(values.targetDate).getTime();
+    let errors = {
+      //   description: "Enter a valid descrdiption",
+      //   targetDate: "Enter a valid target date",
+    };
+    if (values.description.length < 5) errors.description = "Enter at least 5 characters";
+    if (values.targetDate === "") errors.targetDate = "Enter a target date";
+    if (targetDate <= Date.now()) errors.targetDate = "Enter a later date";
+
+    return errors;
   }
   return (
     <div className="container">
       <h1>Enter Todo Details</h1>
       <div>
-        <Formik initialValues={{ description, targetDate }} enableReinitialize={true} onSubmit={onSubmit}>
+        <Formik
+          initialValues={{ description, targetDate }}
+          enableReinitialize={true}
+          onSubmit={onSubmit}
+          validate={validate}
+          validateOnChange={false}
+          validateOnBlur={false}
+        >
           {(props) => (
             <Form>
+              <ErrorMessage name="description" component="div" className="alert alert-warning" />
+              <ErrorMessage name="targetDate" component="div" className="alert alert-warning" />
               <fieldset className="form-group">
                 <label>Description</label>
                 <Field type="text" className="form-control" name="description"></Field>
