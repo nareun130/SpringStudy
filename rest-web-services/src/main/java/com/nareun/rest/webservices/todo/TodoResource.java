ff98249c56b15af85b43d0a202a1f51c1d253878
김성호 <nareun130@gmail.com>
2023-10-26 22:12:14 +0900

    [FEAT] : todo-app 서버에 CRUD 구현

--- rest-web-services/src/main/java/com/nareun/rest/webservices/todo/TodoResource.java
+++ rest-web-services/src/main/java/com/nareun/rest/webservices/todo/TodoResource.java
@@ -1,9 +1,13 @@
 package com.nareun.rest.webservices.todo;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,8 +32,21 @@ public class TodoResource {
     }
 
     @DeleteMapping("/users/{username}/todos/{id}")
-    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id){
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
         todoService.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/users/{username}/todos/{id}")
+    public Todo updateTodo(@PathVariable String username, @PathVariable int id,
+            @RequestBody Todo todo) {
+        todoService.updateTodo(todo);
+        return todo;
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public Todo createTodo(@PathVariable String username, @RequestBody Todo todo) {
+        Todo createdTodo = todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), todo.isDone());
+        return createdTodo;
     }
 }
