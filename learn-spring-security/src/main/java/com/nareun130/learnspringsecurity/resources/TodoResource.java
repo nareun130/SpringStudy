af8d65132fa9cc560cc147f0cfedf85472dc43d5
김성호 <nareun130@gmail.com>
2023-11-01 02:34:07 +0900

    [FEAT] : CSRF 방식 파악

---
+++ learn-spring-security/src/main/java/com/nareun130/learnspringsecurity/resources/TodoResource.java
@@ -0,0 +1,38 @@
+package com.nareun130.learnspringsecurity.resources;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoResource {
+
+	private static final List<Todo> TODOS_LIST = List.of(new Todo("nareun130", "Learn AWS"), new Todo("nareun130", "Get AWS Certified"));
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@GetMapping("/todos")
+	public List<Todo> retrieveAllTodos() {
+		return TODOS_LIST;
+	}
+	
+	@GetMapping("/users/{username}/todos")
+	public Todo retrieveTodosForSpecificUser(@PathVariable String username) {
+		return TODOS_LIST.get(0);
+	}
+	
+	//*Spring Security 사용 시 POST 요청에는 CSRF토큰이 필요 
+	@PostMapping("/users/{username}/todos")
+	public void createTodoForSpecificUser(@PathVariable String username, @RequestBody Todo todo) {
+		logger.info("Create {} for {}", todo, username);
+	}
+}
+
+record Todo(String username, String description) {
+}
