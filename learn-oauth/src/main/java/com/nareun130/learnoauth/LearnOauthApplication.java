44e7685f0f560ac392b47bd5a0923c5493c38a74
김성호 <nareun130@gmail.com>
2023-11-02 01:53:15 +0900

    [FEAT] : Google OAuth 인증 구현

---
+++ learn-oauth/src/main/java/com/nareun130/learnoauth/LearnOauthApplication.java
@@ -0,0 +1,13 @@
+package com.nareun130.learnoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnOauthApplication.class, args);
+	}
+
+}
