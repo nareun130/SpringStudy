b48359c78e3d3eac3c26d9dc14cae7b1d922c924
김성호 <nareun130@gmail.com>
2023-11-02 16:30:58 +0900

    [FEAT] : 공용 포인트 컷 설정

---
+++ learn-spring-aop/src/main/java/com/nareun130/learnspringaop/aop/aspects/CommonPointcutConfig.java
@@ -0,0 +1,28 @@
+package com.nareun130.learnspringaop.aop.aspects;
+
+import org.aspectj.lang.annotation.Pointcut;
+
+public class CommonPointcutConfig {
+	//? 공용 포인트 컷 정의
+	//! 만약 실행 메서드의 위치가 바뀐다면? -> 포인트컷을 일일이 다 정의? 
+	//~> 공용 포인트컷 설정 
+	
+	@Pointcut("execution(* com.nareun130.learnspringaop.aop.*" + ".*.*(..))")
+	public void businessAndDataPackageConfig() {
+		
+	}
+	@Pointcut("execution(* com.nareun130.learnspringaop.aop.business" + ".*.*(..))")
+	public void businessPackageConfig() {
+		
+	}
+	@Pointcut("execution(* com.nareun130.learnspringaop.aop.data" + ".*.*(..))")
+	public void dataPackageConfig() {
+		
+	}
+	
+	@Pointcut("bean(*Service*)") // ? 간단하게 Service가 들어간 bean을 대상으로 해준다.
+	public void allPackageConfigUsringBean() {
+		
+	}
+	
+}
--- learn-spring-aop/src/main/java/com/nareun130/learnspringaop/aop/aspects/LoggingAspect.java
+++ learn-spring-aop/src/main/java/com/nareun130/learnspringaop/aop/aspects/LoggingAspect.java
@@ -19,27 +19,28 @@ public class LoggingAspect {
 	// @Pointcut - When?
 	// ? execution(* PACAKAGE.*.*(..)) : 패키지안의 모든 메서드를 인터셉트 해라
 	// execution(*com.nareun130.learnspringaop.aop.business.*.*(..))
-	@Before("execution(* com.nareun130.learnspringaop.aop.*" + ".*.*(..))")
+	//~> 공용 포인트컷으로 execution설정 
+	@Before("com.nareun130.learnspringaop.aop.aspects.CommonPointcutConfig.allPackageConfigUsringBean()")
 	public void logMethodCallBeforeExecution(JoinPoint joinpoint) {
 		// Logic - What?
-		logger.info("Before Aspect - Method is called {} with arguments : {}", joinpoint, joinpoint.getArgs());
+		logger.info("Before Aspect - {} is called {} with arguments : {}", joinpoint, joinpoint.getArgs());
 
 	}
 
-	@After("execution(* com.nareun130.learnspringaop.aop.*" + ".*.*(..))")
+	@After("com.nareun130.learnspringaop.aop.aspects.CommonPointcutConfig.businessPackageConfig()")
 	public void logMethodCallAfterExecution(JoinPoint joinpoint) {
 		// Logic - What?
 		logger.info("After Aspect - Method is called {} has executed", joinpoint);
 	}
 
-	@AfterThrowing(pointcut = "execution(* com.nareun130.learnspringaop.aop.*" + ".*.*(..))", throwing = "exception")
+	@AfterThrowing(pointcut = "com.nareun130.learnspringaop.aop.aspects.CommonPointcutConfig.businessAndDataPackageConfig()", throwing = "exception")
 	public void logMethodCallAfterException(JoinPoint joinpoint, Exception exception) {
 		//? throwing의 exceptionr과 Exception exception이 연결됨.
 		// Logic - What?
 		logger.info("AfterThrowing Aspect - Method is called {} has thrown an exception {}", joinpoint, exception);
 	}
 	
-	@AfterReturning(pointcut = "execution(* com.nareun130.learnspringaop.aop.*" + ".*.*(..))", returning = "resultValue")
+	@AfterReturning(pointcut = "com.nareun130.learnspringaop.aop.aspects.CommonPointcutConfig.dataPackageConfig()", returning = "resultValue")
 	public void logMethodCallAfterSuccessfulExecution(JoinPoint joinpoint, Object resultValue) {
 		//? throwing의 exceptionr과 Exception exception이 연결됨.
 		// Logic - What?
